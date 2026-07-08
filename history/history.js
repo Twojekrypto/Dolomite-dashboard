d17f803c3633eda9e6db41daded49e734732ea9c
Nat <natina94@gmail.com>
2026-07-08 08:24:48 +0200

    Unify history header with transaction table

--- history/history.js
+++ history/history.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  const HISTORY_VERSION = "history-20260708-table-toolbar-export-menu";
+  const HISTORY_VERSION = "history-20260708-unified-history-card";
   const TAX_REPORT_SCOPE = "Dolomite protocol activity only";
   const TAX_EXTERNAL_COST_BASIS_INCLUDED = "no";
   const TAX_SCOPE_NOTES = "Excludes acquisition cost basis and activity before or after Dolomite.";
